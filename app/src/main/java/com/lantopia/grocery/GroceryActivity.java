4acc51faf6318923afd2b08189fa78710d6b8272
Mark McKenna <mark.mckenna@redspcae.com>
2016-02-24 22:41:43 -0400

    Add proguard

---
+++ app/src/main/java/com/lantopia/grocery/GroceryActivity.java
@@ -0,0 +1,17 @@
+package com.lantopia.grocery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Mark McKenna &lt;mark.mckenna@redspace.com&gt;
+ */
+public class GroceryActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+    }
+}
