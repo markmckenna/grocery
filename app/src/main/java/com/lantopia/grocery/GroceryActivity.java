4769c50bb42736cf5eed3eda1f2b9c596b39375a
Mark McKenna <mark.mckenna@redspcae.com>
2016-02-24 22:51:03 -0400

    Add Android Annotations

--- app/src/main/java/com/lantopia/grocery/GroceryActivity.java
+++ app/src/main/java/com/lantopia/grocery/GroceryActivity.java
@@ -2,10 +2,12 @@ package com.lantopia.grocery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.androidannotations.annotations.EActivity;
 
 /**
  * @author Mark McKenna &lt;mark.mckenna@redspace.com&gt;
  */
+@EActivity(R.layout.grocery_list)
 public class GroceryActivity extends Activity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
